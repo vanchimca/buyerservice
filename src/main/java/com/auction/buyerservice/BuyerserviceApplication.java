5081e93fd8348ebea5bc8dfc330801df8afef61b
vanchimca <vanchi.nat@gmail.com>
2022-03-06 11:43:59 +0530

    updated the cloud urls

--- src/main/java/com/auction/buyerservice/BuyerserviceApplication.java
+++ src/main/java/com/auction/buyerservice/BuyerserviceApplication.java
@@ -2,12 +2,15 @@ package com.auction.buyerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+/*import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;*/
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients("com.auction.buyerservice")
+/*
+ * @EnableEurekaClient
+ * 
+ * @EnableFeignClients("com.auction.buyerservice")
+ */
 public class BuyerserviceApplication {
 
 	public static void main(String[] args) {
